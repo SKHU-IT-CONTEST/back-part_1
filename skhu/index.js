0abcfe373a8c1e77df6adcd71c8c97d41fd86d3b
윤준석 <83647215+junseok0304@users.noreply.github.com>
2022-10-10 14:05:27 +0900

    add Weatherjs

--- skhu/index.js
+++ skhu/index.js
@@ -1,15 +1,27 @@
 const schoolNotice = require("./crawl-lib/schoolNotice.js");
 const express = require("express");
+const Weather = require("./crawl-lib/Weather.js");
 const app = express();
-const jebal = new schoolNotice();
+const noticeObj = new schoolNotice();
+const weatherObj = new Weather();
+const cors = require("cors");
 
+let corsOptions = {
+    origin : '*',
+    Credential : true
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 
 
-app.get("/api/schoolNotice", (req, res) => {
-    jebal.updateData().then((res2) => res.json(res2));
+app.get("/schoolNotice", (req, res) => {
+    noticeObj.getData().then((data) => res.json(data));
+})
+app.get("/getWeather", (req, res) => {
+    weatherObj.getData().then((data) => res.json(data));
 })
 
-app.listen(32023, () => console.log("작동되게 해주세요 ㅠㅠ"));
+app.listen(32023, () => console.log("Listening on port 32023..."));
